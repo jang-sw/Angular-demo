15e225a304e68ba4acca6878bd4a7ca7d8b65f45
jang-sw <jsw118536@gmail.com>
2024-06-24 10:55:35 +0900

    필요없는 부분 정리

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,20 +2,6 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes, UrlSegment } from '@angular/router';
 
 const routes: Routes = [
-  {
-    matcher: (url: UrlSegment[]) => {
-      if (url.length > 0 && url[url.length - 1].path.endsWith('/')) {
-        return {
-          consumed: url,
-          posParams: {
-            slash: new UrlSegment(url[url.length - 1].path.replace(/\/$/, ''), {})
-          }
-        };
-      }
-      return null;
-    },
-    redirectTo: ''
-  }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -28,10 +28,5 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
   bootstrap: [AppComponent]
 })
 export class AppModule {
-  constructor() {
-    const originalStripTrailingSlash = (Location as any).stripTrailingSlash;
-    (Location as any).stripTrailingSlash = function(url: string): string {
-      return url.endsWith('/') && url.length > 1 ? url.slice(0, -1) : url;
-    };
-  }
+  
  }
